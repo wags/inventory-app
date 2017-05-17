06e99e454830a74dbc7e245192db8e7ab263ebd3
Matt Wagner <m.wagner@dwdiesel.com>
2017-05-17 12:55:55 -0400

    Configure products list component
    Add input and output properties, plus an internal property that is only used within the component.

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { Product } from '../product.model';
 
+/**
+ * @ProductsList: A component for rendering all ProductRows and
+ * storing the currently selected Product
+ */
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent {
+  /**
+   * @input productList - the Product[] passed to us
+   */
+  @Input() productList: Product [];
 
-  constructor() { }
+  /**
+   * @output onProductSelected - outputs the currently
+   *          Product whenever a new Product is selected
+   */
+  @Output() onProductSelected: EventEmitter<Product>;
 
-  ngOnInit() {
+  /**
+   * @property currentProduct - local state containing
+   *             the currently selected `Product`
+   */
+  private currentProduct: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
   }
-
 }
