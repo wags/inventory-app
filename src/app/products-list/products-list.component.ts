e5c8a6e679b2c51c25bd444ec31a94282bd7b177
Matt Wagner <m.wagner@dwdiesel.com>
2017-05-17 14:12:41 -0400

    Define clicked and isSelected functions on products list

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -36,4 +36,16 @@ export class ProductsListComponent {
   constructor() {
     this.onProductSelected = new EventEmitter();
   }
+
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
 }
