4820cac79828ea78c493136ba75de44142f79395
Matt Wagner <m.wagner@dwdiesel.com>
2017-05-17 16:02:24 -0400

    Update the product row component

--- src/app/product-row/product-row.component.ts
+++ src/app/product-row/product-row.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  HostBinding
+} from '@angular/core';
+import { Product } from '../product.model';
 
+/**
+ * @ProductRow: a component for the view of a single Product
+ */
 @Component({
   selector: 'app-product-row',
   templateUrl: './product-row.component.html',
   styleUrls: ['./product-row.component.css']
 })
-export class ProductRowComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProductRowComponent {
+  @Input() product: Product;
+  @HostBinding('attr.class') cssClass = 'item';
 }
