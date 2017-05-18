c24d6f206d522334910d19886ef9e690bc0f4c09
Matt Wagner <m.wagner@dwdiesel.com>
2017-05-18 09:36:35 -0400

    Fix typos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,13 +24,13 @@ export class AppComponent {
       new Product(
         'NEATOJACKET',
         'Blue Jacket',
-        '/assets/images/produts/blue-jacket.jpg',
+        '/assets/images/products/blue-jacket.jpg',
         ['Women', 'Apparel', 'Jackets & Vests'],
         238.99),
       new Product(
         'NICEHAT',
         'A Nice Black Hat',
-        '/resources/images/products/black-hat.jpg',
+        '/assets/images/products/black-hat.jpg',
         ['Men', 'Accessories', 'Hats'],
         29.99)
     ];
