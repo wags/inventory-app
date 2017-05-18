edb09f46899111e14a46934b4dd301945188c1a7
Matt Wagner <m.wagner@dwdiesel.com>
2017-05-18 09:25:20 -0400

    Add a product department component

---
+++ src/app/product-department/product-department.component.ts
@@ -0,0 +1,16 @@
+import {
+  Component,
+  Input,
+} from '@angular/core';
+import { Product } from '../product.model';
+
+/**
+ * @ProductDepartment: A component to show the breadcrumbs to a Product's department
+ */
+@Component({
+  selector: 'app-product-department',
+  templateUrl: './product-department.component.html'
+})
+export class ProductDepartmentComponent {
+  @Input() product: Product;
+}
